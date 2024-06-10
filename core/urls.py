e21a415b64c6dc22b6d600224735bc7ce9916ea8
m4hfuj <mahfujmirza273@gmail.com>
2024-06-10 12:50:10 +0600

    railway postgre

--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,4 @@ urlpatterns = [
 
     path('', include('store.urls')),
     path('', include('admin_panel.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
